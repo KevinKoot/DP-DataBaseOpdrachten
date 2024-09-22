668d94dae361571b59eef69622ad1b3472d1d645
Kevin <Kevin.koot@student.hu.nl>
2024-09-22 15:52:21 +0200

    opdracht 4

--- opdracht/S4_Advanced_SQL_student.sql
+++ opdracht/S4_Advanced_SQL_student.sql
@@ -5,7 +5,7 @@
 -- (c) 2020 Hogeschool Utrecht
 -- Tijmen Muller (tijmen.muller@hu.nl)
 -- André Donk (andre.donk@hu.nl)
--- 
+--
 --
 -- Opdracht: schrijf SQL-queries om onderstaande resultaten op te vragen,
 -- aan te maken, verwijderen of aan te passen in de database van de
@@ -26,55 +26,93 @@
 -- ------------------------------------------------------------------------
 
 
--- S4.1. 
+-- S4.1.
 -- Geef nummer, functie en geboortedatum van alle medewerkers die vóór 1980
 -- geboren zijn, en trainer of verkoper zijn.
--- DROP VIEW IF EXISTS s4_1; CREATE OR REPLACE VIEW s4_1 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s4_1; CREATE OR REPLACE VIEW s4_1 AS
+--
+select mnr, functie, gbdatum
+from medewerkers
+where gbdatum < '1980-01-01'
+  and (functie = 'TRAINER' or functie = 'VERKOPER')
 
 
--- S4.2. 
+-- S4.2.
 -- Geef de naam van de medewerkers met een tussenvoegsel (b.v. 'van der').
--- DROP VIEW IF EXISTS s4_2; CREATE OR REPLACE VIEW s4_2 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s4_2; CREATE OR REPLACE VIEW s4_2 AS
+--
+select naam
+from medewerkers
+where naam like '% %'
 
 
--- S4.3. 
+-- S4.3.
 -- Geef nu code, begindatum en aantal inschrijvingen (`aantal_inschrijvingen`) van alle
 -- cursusuitvoeringen in 2019 met minstens drie inschrijvingen.
--- DROP VIEW IF EXISTS s4_3; CREATE OR REPLACE VIEW s4_3 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s4_3; CREATE OR REPLACE VIEW s4_3 AS
+--
+select inschrijving.cursus, inschrijving.begindatum, count(*) as aantal_inschrijvingen
+from inschrijvingen as inschrijving
+group by inschrijving.cursus, inschrijving.begindatum
+having count(*) >= 3
+   and inschrijving.begindatum between '2019-01-01' and '2020-01-01';
 
 
--- S4.4. 
+
+-- S4.4.
 -- Welke medewerkers hebben een bepaalde cursus meer dan één keer gevolgd?
--- Geef medewerkernummer en cursuscode.
--- DROP VIEW IF EXISTS s4_4; CREATE OR REPLACE VIEW s4_4 AS                                                     -- [TEST]
+-- Geef medewerkersnummer en cursuscode.
+-DROP VIEW IF EXISTS s4_4; CREATE OR REPLACE VIEW s4_4 AS
+
+select medewerkers.mnr, inschrijvingen.cursus
+from inschrijvingen
+         join medewerkers on inschrijvingen.cursist = medewerkers.mnr
+group by medewerkers.mnr, inschrijvingen.cursus
+having count(inschrijvingen.cursus) > 1;
+-- [TEST]
 
 
--- S4.5. 
+-- S4.5.
 -- Hoeveel uitvoeringen (`aantal`) zijn er gepland per cursus?
 -- Een voorbeeld van het mogelijke resultaat staat hieronder.
 --
---   cursus | aantal   
+--   cursus | aantal
 --  --------+-----------
---   ERM    | 1 
---   JAV    | 4 
---   OAG    | 2 
--- DROP VIEW IF EXISTS s4_5; CREATE OR REPLACE VIEW s4_5 AS                                                     -- [TEST]
+--   ERM    | 1
+--   JAV    | 4
+--   OAG    | 2
+DROP VIEW IF EXISTS s4_5; CREATE OR REPLACE VIEW s4_5 AS
+--
+select cursus, count(cursus) as aantal_cursussen from uitvoeringen
+group by cursus
 
 
--- S4.6. 
--- Bepaal hoeveel jaar leeftijdsverschil er zit tussen de oudste en de 
+-- S4.6.
+-- Bepaal hoeveel jaar leeftijdsverschil er zit tussen de oudste en de
 -- jongste medewerker (`verschil`) en bepaal de gemiddelde leeftijd van
 -- de medewerkers (`gemiddeld`).
 -- Je mag hierbij aannemen dat elk jaar 365 dagen heeft.
--- DROP VIEW IF EXISTS s4_6; CREATE OR REPLACE VIEW s4_6 AS                                                     -- [TEST]
+-- DROP VIEW IF EXISTS s4_6; CREATE OR REPLACE VIEW s4_6 AS
+--
+select
+    (max(gbdatum) - min(gbdatum)) / 365 AS verschil_in_jaren,
+    round(2024 - avg(extract(year from gbdatum))) as gemiddelde_leeftijd
+from medewerkers;
 
 
--- S4.7. 
+-- S4.7.
 -- Geef van het hele bedrijf een overzicht van het aantal medewerkers dat
 -- er werkt (`aantal_medewerkers`), de gemiddelde commissie die ze
 -- krijgen (`commissie_medewerkers`), en hoeveel dat gemiddeld
 -- per verkoper is (`commissie_verkopers`).
--- DROP VIEW IF EXISTS s4_7; CREATE OR REPLACE VIEW s4_7 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s4_7; CREATE OR REPLACE VIEW s4_7 AS
+
+select count(mnr) as aantal_medewerkers,
+       (sum(comm)) / count(mnr) as commissie_verkoper,
+       (sum(comm)) / nullif(count(comm), 0) as commissie_medewerkers
+from medewerkers
+
+
 
 
 
