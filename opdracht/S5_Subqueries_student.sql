99090dc2d17b650c392e92cadf2066371c775c23
Kevin <Kevin.koot@student.hu.nl>
2024-10-06 22:38:04 +0200

    .

--- opdracht/S5_Subqueries_student.sql
+++ opdracht/S5_Subqueries_student.sql
@@ -31,50 +31,119 @@
 -- S5.1.
 -- Welke medewerkers hebben zowel de Java als de XML cursus
 -- gevolgd? Geef hun personeelsnummers.
--- DROP VIEW IF EXISTS s5_1; CREATE OR REPLACE VIEW s5_1 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_1;
+CREATE OR REPLACE VIEW s5_1 AS
+SELECT cursist
+FROM inschrijvingen
+WHERE cursus = 'JAV'
+  AND cursist IN
+      (SELECT cursist
+       FROM inschrijvingen
+       WHERE cursus = 'XML');
+
 
 
 -- S5.2.
 -- Geef de nummers van alle medewerkers die niet aan de afdeling 'OPLEIDINGEN'
 -- zijn verbonden.
--- DROP VIEW IF EXISTS s5_2; CREATE OR REPLACE VIEW s5_2 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_2;
+CREATE OR REPLACE VIEW s5_2 AS
+SELECT mnr
+FROM medewerkers
+         JOIN afdelingen ON medewerkers.afd = afdelingen.anr
+WHERE NOT afdelingen.naam = 'OPLEIDINGEN';                                                  -- [TEST]
 
 
 -- S5.3.
 -- Geef de nummers van alle medewerkers die de Java-cursus niet hebben
 -- gevolgd.
--- DROP VIEW IF EXISTS s5_3; CREATE OR REPLACE VIEW s5_3 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_3;
+CREATE OR REPLACE VIEW s5_3 AS
+SELECT DISTINCT medewerkers.mnr
+FROM medewerkers
+WHERE medewerkers.mnr NOT IN
+      (SELECT cursist
+       FROM inschrijvingen
+       WHERE cursus = 'JAV');                                                    -- [TEST]
 
 
 -- S5.4.
 -- a. Welke medewerkers hebben ondergeschikten? Geef hun naam.
--- DROP VIEW IF EXISTS s5_4a; CREATE OR REPLACE VIEW s5_4a AS                                                   -- [TEST]
+DROP VIEW IF EXISTS s5_4a;
+CREATE OR REPLACE VIEW s5_4a AS
+SELECT DISTINCT naam
+FROM medewerkers
+WHERE medewerkers.mnr IN (SELECT chef
+                          FROM medewerkers);                                                 -- [TEST]
 
 -- b. En welke medewerkers hebben geen ondergeschikten? Geef wederom de naam.
--- DROP VIEW IF EXISTS s5_4b; CREATE OR REPLACE VIEW s5_4b AS                                                   -- [TEST]
+DROP VIEW IF EXISTS s5_4b;
+CREATE OR REPLACE VIEW s5_4b AS
+SELECT naam
+FROM medewerkers
+WHERE medewerkers.mnr NOT IN
+      (SELECT chef
+       FROM medewerkers
+       WHERE chef IS NOT NULL);                                                -- [TEST]
 
 
 -- S5.5.
 -- Geef cursuscode en begindatum van alle uitvoeringen van programmeercursussen
 -- ('BLD') in 2020.
--- DROP VIEW IF EXISTS s5_5; CREATE OR REPLACE VIEW s5_5 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_5;
+CREATE OR REPLACE VIEW s5_5 AS
+SELECT cursus, begindatum
+FROM uitvoeringen
+WHERE cursus IN (SELECT code
+                 FROM cursussen
+                 WHERE type = 'BLD')
+  AND begindatum BETWEEN '2020-01-01' AND '2020-12-31';                                                   -- [TEST]
 
 
 -- S5.6.
 -- Geef van alle cursusuitvoeringen: de cursuscode, de begindatum en het
 -- aantal inschrijvingen (`aantal_inschrijvingen`). Sorteer op begindatum.
--- DROP VIEW IF EXISTS s5_6; CREATE OR REPLACE VIEW s5_6 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_6;
+CREATE OR REPLACE VIEW s5_6 AS
+SELECT uitvoeringen.cursus,
+       uitvoeringen.begindatum,
+       COUNT(inschrijvingen.cursus) AS aantal_inschrijvingen
+FROM uitvoeringen
+         LEFT JOIN inschrijvingen ON uitvoeringen.cursus = inschrijvingen.cursus
+    AND uitvoeringen.begindatum = inschrijvingen.begindatum
+GROUP BY uitvoeringen.cursus,
+         uitvoeringen.begindatum
+ORDER BY uitvoeringen.begindatum;                                                   -- [TEST]
 
 
 -- S5.7.
 -- Geef voorletter(s) en achternaam van alle trainers die ooit tijdens een
 -- algemene ('ALG') cursus hun eigen chef als cursist hebben gehad.
--- DROP VIEW IF EXISTS s5_7; CREATE OR REPLACE VIEW s5_7 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_7;
+CREATE OR REPLACE VIEW s5_7 AS
+SELECT voorl, naam
+FROM medewerkers
+WHERE functie = 'TRAINER'
+  AND medewerkers.mnr IN
+      (SELECT docent
+       FROM uitvoeringen
+       WHERE cursus IN (SELECT code
+                        FROM cursussen
+                        WHERE type = 'ALG')
+         AND code IN (SELECT cursus
+                      FROM inschrijvingen
+                      WHERE cursist = medewerkers.chef));                                                    -- [TEST]
 
 
 -- S5.8.
 -- Geef de naam van de medewerkers die nog nooit een cursus hebben gegeven.
--- DROP VIEW IF EXISTS s5_8; CREATE OR REPLACE VIEW s5_8 AS                                                     -- [TEST]
+DROP VIEW IF EXISTS s5_8;
+CREATE OR REPLACE VIEW s5_8 AS
+SELECT naam
+FROM medewerkers
+WHERE mnr NOT IN (SELECT docent
+                  FROM uitvoeringen
+                  WHERE docent IS NOT NULL);                                                   -- [TEST]
 
 
 
--- opdracht/S6_Views_student.sql
+++ opdracht/S6_Views_student.sql
@@ -9,21 +9,60 @@
 
 
 -- S6.1.
---
 -- 1. Maak een view met de naam "deelnemers" waarmee je de volgende gegevens uit de tabellen inschrijvingen en uitvoering combineert:
 --    inschrijvingen.cursist, inschrijvingen.cursus, inschrijvingen.begindatum, uitvoeringen.docent, uitvoeringen.locatie
+
+CREATE OR REPLACE VIEW deelnemers AS
+SELECT  inschrijvingen.cursist,
+        inschrijvingen.cursus,
+        inschrijvingen.begindatum,
+        uitvoeringen.docent,
+        uitvoeringen.locatie
+FROM inschrijvingen
+         JOIN uitvoeringen ON inschrijvingen.cursus = uitvoeringen.cursus
+    AND inschrijvingen.begindatum = uitvoeringen.begindatum;
+
 -- 2. Gebruik de view in een query waarbij je de "deelnemers" view combineert met de "personeels" view (behandeld in de les):
---     CREATE OR REPLACE VIEW personeel AS
--- 	     SELECT mnr, voorl, naam as medewerker, afd, functie
---       FROM medewerkers;
--- 3. Is de view "deelnemers" updatable ? Waarom ?
+CREATE OR REPLACE VIEW personeel AS
+SELECT mnr, voorl, naam AS medewerker, afd, functie
+FROM medewerkers;
+
+SELECT deelnemers.cursist,
+       deelnemers.cursus,
+       deelnemers.begindatum,
+       personeel.medewerker AS docent_naam,
+       personeel.afd AS docent_afdeling,
+       deelnemers.locatie
+FROM deelnemers
+         JOIN personeel ON deelnemers.docent = personeel.mnr;
+
+-- 3. Is de view "deelnemers" updatable? Waarom?
+-- Nee, omdat de view gebaseerd is op meerdere tabellen, waar een JOIN is gebruikt.
+nee, omdat de view gebaseerd is meerdere tafels, waar ik join heb gebruik
 
 
 -- S6.2.
---
--- 1. Maak een view met de naam "dagcursussen". Deze view dient de gegevens op te halen: 
---      code, omschrijving en type uit de tabel curssussen met als voorwaarde dat de lengte = 1. Toon aan dat de view werkt. 
--- 2. Maak een tweede view met de naam "daguitvoeringen". 
---    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcurssussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt
--- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP view <viewnaam> CASCADE en bij DROP view <viewnaam> RESTRICT
+-- 1. Maak een view met de naam "dagcursussen". Deze view dient de gegevens op te halen:
+--    code, omschrijving en type uit de tabel cursussen met als voorwaarde dat de lengte = 1. Toon aan dat de view werkt.
+CREATE OR REPLACE VIEW dagcursussen AS
+SELECT code, omschrijving, type
+FROM cursussen
+WHERE lengte = 1;
 
+-- 2. Maak een tweede view met de naam "daguitvoeringen".
+--    Deze view dient de uitvoeringsgegevens op te halen voor de "dagcursussen" (gebruik ook de view "dagcursussen"). Toon aan dat de view werkt.
+CREATE OR REPLACE VIEW daguitvoeringen AS
+SELECT uitvoeringen.cursus,
+       uitvoeringen.begindatum,
+       uitvoeringen.locatie,
+       uitvoeringen.docent
+FROM uitvoeringen
+         JOIN dagcursussen ON uitvoeringen.cursus = dagcursussen.code;
+
+-- 3. Verwijder de views en laat zien wat de verschillen zijn bij DROP VIEW <viewnaam> CASCADE en bij DROP VIEW <viewnaam> RESTRICT
+DROP VIEW IF EXISTS daguitvoeringen CASCADE;
+DROP VIEW IF EXISTS dagcursussen CASCADE;
+
+ Bij RESTRICT verwijder je de view alleen als deze niet afhankelijk is van andere objecten,
+
+ terwijl je bij CASCADE ook alle afhankelijkheden erbij verwijderd.
